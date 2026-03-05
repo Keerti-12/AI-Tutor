827ea438548807b40478e474b014fb4145be67f3
Keerti-12 <kirtyyaduvanshi@gmail.com>
2026-03-05 16:33:39 +0530

    fixed api issue

--- pages/api/cohere.js
+++ pages/api/cohere.js
@@ -1,4 +1,6 @@
 // Next.js API route to proxy requests to Cohere
+import fetch from 'node-fetch';
+
 export default async function handler(req, res) {
   if (req.method !== 'POST') {
     return res.status(405).json({ error: 'Method not allowed' });
